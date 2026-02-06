7b624f6dfe5dd9c85af0322813ec7dffb28a91d4
oceane.huynh <oceanehuynh711@gmail.com>
2026-02-06 14:53:36 +0100

    premier commit

---
+++ app/src/main/java/iut/dam/powerhome/LoginActivity.java
@@ -0,0 +1,48 @@
+package iut.dam.powerhome;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.loginactivity);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginactivity), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void login(View v){
+        EditText editEmail = findViewById(R.id.email);
+        EditText editPassword = findViewById(R.id.password);
+
+        String email = editEmail.getText().toString();
+        String password = editPassword.getText().toString();
+
+        if(password.equals("EFGH") && email.equals("abcd")){
+            Intent intent = new Intent(this, MainActivity.class);
+
+            intent.putExtra("email", email);
+            intent.putExtra("mdp", password);
+
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(this, "Identifiants incorrects", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/iut/dam/powerhome/RegisterActivity.java
@@ -0,0 +1,33 @@
+package iut.dam.powerhome;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.registeractivity);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Spinner planetsSP = findViewById(R.id.sp_planets);
+        String[] items = {"+1", "+33", "+34"};
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(this,
+                        android.R.layout.simple_list_item_1,
+                        items);
+        planetsSP.setAdapter(adapter);
+    }
+}
