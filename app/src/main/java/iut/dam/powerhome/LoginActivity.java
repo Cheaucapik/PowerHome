055fcfa6e3f79c996fd120e41b9f8e7023e759cb
Cheaucapik <oceane.huynh@etu.u-paris.fr>
2026-02-24 00:03:25 +0100

    Splash screen

--- app/src/main/java/iut/dam/powerhome/LoginActivity.java
+++ app/src/main/java/iut/dam/powerhome/LoginActivity.java
@@ -2,21 +2,48 @@ package iut.dam.powerhome;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 
 public class LoginActivity extends AppCompatActivity {
+    private boolean isReady = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
+        final View content = findViewById(android.R.id.content);
+        content.getViewTreeObserver().addOnPreDrawListener(
+                new ViewTreeObserver.OnPreDrawListener() {
+                    @Override
+                    public boolean onPreDraw() {
+                        if (isReady) {
+                            content.getViewTreeObserver().removeOnPreDrawListener(this);
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+        );
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            isReady = true;
+        }, 1000);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.loginactivity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginactivity), (v, insets) -> {
