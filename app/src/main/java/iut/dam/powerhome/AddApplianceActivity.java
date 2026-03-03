ae34b3c0eefb85c80d230a507eda84aa0b5859a5
Cheaucapik <oceane.huynh@etu.u-paris.fr>
2026-03-03 14:42:57 +0100

    fin ajout appliance après sign up & début modification appliances

--- app/src/main/java/iut/dam/powerhome/AddApplianceActivity.java
+++ app/src/main/java/iut/dam/powerhome/AddApplianceActivity.java
@@ -1,15 +1,42 @@
 package iut.dam.powerhome;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddApplianceActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.add_appliance_signup_activity);
+        TextView name_tv = findViewById(R.id.name_tv);
+
+        SharedPreferences sp = getSharedPreferences("UserSession", MODE_PRIVATE);
+        String json = sp.getString("user_json", null);
+
+        if (json != null) {
+            User currentUser = User.getFromJson(json);
+            String name = currentUser.firstname + " " + currentUser.lastname;
+            name_tv.setText(getString(R.string.welcome) + name);
+        }
+    }
+
+    public void add(View v){
+        CustomDialogAddAppliance custom = new CustomDialogAddAppliance(AddApplianceActivity.this);
+        custom.show();
+    }
+
+    public void later(View v){
+        Intent intent = new Intent(AddApplianceActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
